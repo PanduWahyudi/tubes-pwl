42efc586ed47a8d828ebf008b65339ca30c923ce
PanduWahyudi <panduwahyudi12@gmail.com>
2023-12-05 01:15:09 +0700

    Setup

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl font-bold"> Home Page </h1>
+    </div>
+  );
+};
+
+export default Home;
