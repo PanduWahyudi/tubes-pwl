42ffd5b714fb970531b4d903122eb77e0d574e16
PanduWahyudi <panduwahyudi12@gmail.com>
2023-12-05 01:22:28 +0700

    Tes set up

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="flex justify-center items-center">
-      <h1 className="text-3xl font-bold"> Home Page </h1>
+      <h1 className="text-3xl font-bold text-red-600"> Home Page </h1>
     </div>
   );
 };
